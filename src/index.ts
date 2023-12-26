4157109153e4f68316086a1ed60415399865141f
satish <satish860@gmail.com>
2023-12-26 12:56:04 +0530

    Added the Put color correctly

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+type Bindings = {
+    MY_BUCKET: R2Bucket
+  }
+
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', (c) => c.text('Hello Hono!'))
+
+app.put('/upload/:key', async (c, next) => {
+    const key = c.req.param('key')
+    await c.env.MY_BUCKET.put(key, c.req.raw.body)
+    return c.text(`Put ${key} successfully!`)
+})
 
 export default app
