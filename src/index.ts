4ecbef72b47fdce2956364b577291bf30364230d
apprakash <101494351+apprakash@users.noreply.github.com>
2023-12-28 20:01:21 +0530

    Changed to accomodate folder upload. CORS method included.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,41 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { cors } from 'hono/cors';
 
 type Bindings = {
-    MY_BUCKET: R2Bucket
+  MY_BUCKET: R2Bucket;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+app.use('*', cors());
+
+app.get("/", (c) => c.text("Hello Hono!"));
+
+app.put("/upload/*", async (c) => {
+  const fullPath = c.req.path.replace('/upload/', '');
+
+  if (typeof fullPath !== "string") {
+    return c.text("Invalid path", 400);
+  }
+  
+  const segments = fullPath.split("/");
+  const filename = segments.pop();
+  const folder = segments.join("/");
+  const key = folder ? `${folder}/${filename}` : filename;
+
+  const contentType = c.req.header("Content-Type");
+
+  const blob = await c.req.blob();
+  if (blob.size === 0) {
+    return c.text("No file uploaded", 400);
   }
 
-const app = new Hono<{ Bindings: Bindings }>()
+  await c.env.MY_BUCKET.put(key!, blob.stream(), {
+    httpMetadata: {
+      contentType: contentType,
+    },
+  });
 
-app.get('/', (c) => c.text('Hello Hono!'))
+  return c.text(`File uploaded to ${key} successfully!`);
+});
 
-app.put('/upload/:key', async (c, next) => {
-    const key = c.req.param('key')
-    await c.env.MY_BUCKET.put(key, c.req.raw.body)
-    return c.text(`Put ${key} successfully!`)
-})
-
-export default app
+export default app;
